b64c75114d16ac0b2bf435bbbc232130177ecc50
konchellah <williamoriginal2019@gmail.com>
2023-02-11 20:14:41 +0300

    Added messaging

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const { PubSub } = require('@google-cloud/pubsub');
+
+const pubsub = new PubSub();
+
+const subscription =  pubsub.createSubscription("appointment-subscription")
+
+subscription.on('message', message => {
+  console.log('Received message:', message.data.toString());
+  process.exit(0);
+});
+
+
+subscription.on('error', error => {
+  console.error('Received error:', error);
+  process.exit(1);
+});
+
+
+
 app.get('/', (req, res) => {
   res.status(200).send("I am  here");
 });
@@ -14,5 +33,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(8000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on 8000');
 });
