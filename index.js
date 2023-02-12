b27703fad4130215040bb595eec92a65761c52db
konchellah <williamoriginal2019@gmail.com>
2023-02-12 17:28:39 +0300

    Cors and Push Subscription

--- index.js
+++ index.js
@@ -3,42 +3,72 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
-const { PubSub } = require('@google-cloud/pubsub');
-
-const pubsub = new PubSub({
-  projectId: 'zendeta',
-  keyFilename: './keyfile.json'
+const cors = require('cors')
+const io = new Server(server, {
+  cors: true
 });
 
 
-const subscriptionName = 'appointment-subscription';
+//below code is for pull subscription
+// // const { PubSub } = require('@google-cloud/pubsub');
+
+// // const pubsub = new PubSub({
+// //   projectId: 'zendeta',
+// //   keyFilename: './keyfile.json'
+// // });
 
 
-function listenForMessages() {
-  // References an existing subscription
-  const subscription = pubsub.subscription(subscriptionName);
+// // const subscriptionName = 'appointment-subscription';
 
-  // Create an event handler to handle messages
+
+// // function listenForMessages() {
+// //   // References an existing subscription
+// //   const subscription = pubsub.subscription(subscriptionName);
+
+// //   // Create an event handler to handle messages
   
-  const messageHandler = message => {
-    console.log(`Received message ${message.id}:`);
-    console.log(`\tData: ${message.data}`);
-    console.log(`\tAttributes: ${message.attributes}`);
-    messageCount += 1;
+// //   const messageHandler = message => {
+// //     console.log(`Received message ${message.id}:`);
+// //     console.log(`\tData: ${message.data}`);
+// //     console.log(`\tAttributes: ${message.attributes}`);
+    
 
-    // "Ack" (acknowledge receipt of) the message
-    message.ack();
-  };
+// //     // "Ack" (acknowledge receipt of) the message
+// //     message.ack();
+// //   };
 
-  // Listen for new messages until timeout is hit
-  subscription.on('message', messageHandler);
+// //   // Listen for new messages until timeout is hit
+// //   subscription.on('message', messageHandler);
 
  
-}
+// // }
 
-listenForMessages();
+// // listenForMessages();
+
+//push subscription
+
+app.post('/pub-sub-messages', (req, res) => {
+  if (!req.body) {
+    const msg = 'no Pub/Sub message received';
+    console.error(`error: ${msg}`);
+    res.status(400).send(`Bad Request: ${msg}`);
+    return;
+  }
+  if (!req.body.message) {
+    const msg = 'invalid Pub/Sub message format';
+    console.error(`error: ${msg}`);
+    res.status(400).send(`Bad Request: ${msg}`);
+    return;
+  }
+
+  const pubSubMessage = req.body.message;
+  const name = pubSubMessage.data
+    ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
+    : 'World';
+
+  console.log(`Hello ${name}!`);
+  res.status(204).send();
+});
 
 app.get('/', (req, res) => {
   res.status(200).send("I am  here");
